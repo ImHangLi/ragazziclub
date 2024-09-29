8a8b9717aea77f7ccfd9553846a65ea5066231b7
Jeff Zhang <z5591223@ad.unsw.edu.au>
2024-09-29 21:51:19 +1000

    Add provision for example listing

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
-      <a href="/" className="logo">
-        RAGAZZICLUB
-      </a>
+      <a href="/" className="logo">RAGAZZICLUB</a>
       <nav>
-        <a href="/cars-for-sale">Cars for Sale</a>
-        <a href="/research">Research</a>
-        <a href="/contact">Contact Us</a>
+        <Link to="/cars-for-sale">Cars for Sale</Link>
+        <Link to="/listing">Example Car Listing</Link> {/* New link */}
+        <Link to="/research">Research</Link>
+        <Link to="/contact">Contact Us</Link>
       </nav>
     </header>
-  )
+  );
 }
